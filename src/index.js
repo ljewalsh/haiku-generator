cc0a87a7bd71eebe2399916ef6dce8ab3f2a965e
Lucy-Jane Walsh <ljewalsh@gmail.com>
2018-11-25 22:48:45 +1300

    Updates index

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-import { CronJob } from 'cron'
 import createHaiku from './haiku/createHaiku'
 import { createTable, saveItem } from './database'
 import keys from '../twitterKeys.json'
 import createTwitterClient from './tweets/createTwitterClient'
+import postTweet from './tweets/postTweet'
 
 const twitterClient = createTwitterClient(keys)
 
-const job = new CronJob({
-  cronTime: '7-59/15 * * * *',
-  onTick: async () => {
+const runGenerator = async () => {
+  const run = true
+  while (run) {
+    await createTable('haikus')
+    await createTable('tweets')
+    await createTable('requests')
     const haiku = await createHaiku(twitterClient)
-    await saveItem('haikus', haiku)
+    await saveItem('haikus', { text: haiku })
     console.log(haiku)
-  },
-      start: false
-})
+    await postTweet({ client: twitterClient, tweet: haiku })
+  }
+}
 
-createTable('haikus').then(
-  createTable('tweets').then(
-      job.start()
-  )
-)
-
+runGenerator().then()
